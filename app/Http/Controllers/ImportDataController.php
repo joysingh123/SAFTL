3cd999fda40a2fe6ce286a855e0a0e119a82773b
joysingh123 <joy@salesaladin.com>
2018-09-05 22:45:45 +0530

    script max execution time excedded

--- app/Http/Controllers/ImportDataController.php
+++ app/Http/Controllers/ImportDataController.php
@@ -120,7 +120,7 @@ class ImportDataController extends Controller {
     }
 
     public function importContactData(Request $request) {
-        ini_set('max_execution_time', 300);
+        ini_set('max_execution_time', 600);
         ini_set('memory_limit', -1);
         $this->validate($request, array(
             'file' => 'required'
