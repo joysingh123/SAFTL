4b6214fe4c03769e4374194a76ed1f9b23557bc7
joysingh123 <joy@salesaladin.com>
2018-09-06 22:14:18 +0530

    debugging

--- app/Http/Controllers/ImportDataController.php
+++ app/Http/Controllers/ImportDataController.php
@@ -29,26 +29,27 @@ class ImportDataController extends Controller {
     }
 
     public function importComapniesWithDomainData(Request $request) {
-        ini_set('max_execution_time', 600);
-        ini_set('memory_limit', -1);
-        ini_set('mysql.connect_timeout', 300);
-        ini_set('default_socket_timeout', 300);
-        $this->validate($request, array(
-            'file' => 'required'
-        ));
-        
-        if ($request->hasFile('file')) {
-            $extension = File::extension($request->file->getClientOriginalName());
-            if ($extension == "xlsx" || $extension == "xls") {
-                $path = $request->file->getRealPath();
-                $data = Excel::load($path, function($reader) {})->get();
-                echo $data;
-                
-            } else {
-                Session::flash('error', 'File is a ' . $extension . ' file.!! Please upload a valid xls file..!!');
-                return back();
-            }
-        }
+        dd($request->input());
+//        ini_set('max_execution_time', 600);
+//        ini_set('memory_limit', -1);
+//        ini_set('mysql.connect_timeout', 300);
+//        ini_set('default_socket_timeout', 300);
+//        $this->validate($request, array(
+//            'file' => 'required'
+//        ));
+//        
+//        if ($request->hasFile('file')) {
+//            $extension = File::extension($request->file->getClientOriginalName());
+//            if ($extension == "xlsx" || $extension == "xls") {
+//                $path = $request->file->getRealPath();
+//                $data = Excel::load($path, function($reader) {})->get();
+//                echo $data;
+//                
+//            } else {
+//                Session::flash('error', 'File is a ' . $extension . ' file.!! Please upload a valid xls file..!!');
+//                return back();
+//            }
+//        }
     }
 
     public function importContactData(Request $request) {
