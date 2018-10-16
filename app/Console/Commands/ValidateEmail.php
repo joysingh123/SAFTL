5fdfd661cfb7fd4d51215dcbdc6dd224e0da3983
joysingh123 <joy@salesaladin.com>
2018-10-16 20:48:31 +0530

    limit increases

--- app/Console/Commands/ValidateEmail.php
+++ app/Console/Commands/ValidateEmail.php
@@ -49,7 +49,7 @@ class ValidateEmail extends Command {
         ini_set('mysql.connect_timeout', 600);
         ini_set('default_socket_timeout', 600);
         $response = array();
-        $limit = 10;
+        $limit = 250;
         $emails = DB::table('emails')
                         ->select('matched_contact_id', DB::raw("group_concat(email) AS emails"))
                         ->groupBy('matched_contact_id')
