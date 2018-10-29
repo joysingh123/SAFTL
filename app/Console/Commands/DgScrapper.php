a9dfc43db77e7f598449537f919eb3e988ba5dd0
joysingh123 <joy@salesaladin.com>
2018-10-29 18:31:19 +0530

    scrape url added for dg scrapper

--- app/Console/Commands/DgScrapper.php
+++ app/Console/Commands/DgScrapper.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\DgCompanies;
+use App\DgUrl;
 use App\Helpers\UtilDebug;
 use DB;
 class DgScrapper extends Command
@@ -46,59 +47,57 @@ class DgScrapper extends Command
         ini_set('default_socket_timeout', 600);
         
         $scrapper_base_url  = "https://discoverorg.com";
-        $scrapper_list_array = array(
-                                        'L'=>23,
-                                        'M'=>356,
-                                        'N'=>237,
-                                        'O'=>399,
-                                        'P'=>158,
-                                        'Q'=>165,
-                                        'R'=>144,
-                                        'S'=>149,
-                                        'T'=>154,
-                                        'U'=>148,
-                                        'V'=>58,
-                                        'W'=>87,
-                                        'X'=>87,
-                                        'Y'=>87,
-                                        'Z'=>87
-                                    );
-        foreach ($scrapper_list_array AS $k=>$pagination){
-            $list_url = $scrapper_base_url."/directory/company/list/$k";
-            $html = new \Htmldom($list_url);
-            $data = $html->find('div.container-fluid > div.contact-section > div.mt-3 > div.directory-row');
-            $result = array();
-            foreach($data AS $d){
-                $first_child = $d->find('div.pr-2 > div.directory-entry > a');
-                if(isset($first_child[0])){
-                    $first_child_loc = $d->find('div.pr-2 > div.directory-entry > span');
-                    $link =  $scrapper_base_url.$first_child[0]->href;
-                    $name =  htmlspecialchars_decode($first_child[0]->plaintext);
-                    $location =  $first_child_loc[0]->plaintext;
-                    $html_view_source = new \Htmldom($link);
-                    $html_view_source = $html_view_source->find('div.site-container');
-                    $view_source = $html_view_source[0];
-                    $result[] = ["Company_Name" => $name, "Company_Url" => $link,"Location"=>$location,'View_Source'=>$view_source];  
+        $limit = 1;
+        $url = DgUrl::where('status','not processed')->take($limit)->get();
+        if($url->count() > 0){
+            $url = $url->first();
+            $url_param = $url->url_param;
+            $pagination = $url->pagination;
+            $processed = FALSE;
+            for($i=2;$i <= $pagination;$i++){
+                $list_url = $scrapper_base_url."/directory/company/list/$url_param?p=$i";
+                $html = new \Htmldom($list_url);
+                $data = $html->find('div.container-fluid > div.contact-section > div.mt-3 > div.directory-row');
+                $result = array();
+                foreach($data AS $d){
+                    $first_child = $d->find('div.pr-2 > div.directory-entry > a');
+                    if(isset($first_child[0])){
+                        $first_child_loc = $d->find('div.pr-2 > div.directory-entry > span');
+                        $link =  $scrapper_base_url.$first_child[0]->href;
+                        $name =  htmlspecialchars_decode($first_child[0]->plaintext);
+                        $location =  $first_child_loc[0]->plaintext;
+                        $html_view_source = new \Htmldom($link);
+                        $html_view_source = $html_view_source->find('div.site-container');
+                        $view_source = $html_view_source[0];
+                        $result[] = ["Company_Name" => $name, "Company_Url" => $link,"Location"=>$location,'View_Source'=>$view_source];  
+                    }
+                    $second_child = $d->find('div.pl-2 > div.directory-entry > a');
+                    if(isset($second_child[0])){
+                        $second_child_loc = $d->find('div.pl-2 > div.directory-entry > span');
+                        $link =  $scrapper_base_url.$second_child[0]->href;
+                        $name =  htmlspecialchars_decode($second_child[0]->plaintext);
+                        $location =  $second_child_loc[0]->plaintext;
+                        $html_view_source = new \Htmldom($link);
+                        $html_view_source = $html_view_source->find('div.site-container');
+                        $view_source = $html_view_source[0];
+                        $result[] = ["Company_Name" => $name, "Company_Url" => $link,"Location"=>$location,'View_Source'=>$view_source];  
+                    }  
                 }
-                $second_child = $d->find('div.pl-2 > div.directory-entry > a');
-                if(isset($second_child[0])){
-                    $second_child_loc = $d->find('div.pl-2 > div.directory-entry > span');
-                    $link =  $scrapper_base_url.$second_child[0]->href;
-                    $name =  htmlspecialchars_decode($second_child[0]->plaintext);
-                    $location =  $second_child_loc[0]->plaintext;
-                    $html_view_source = new \Htmldom($link);
-                    $html_view_source = $html_view_source->find('div.site-container');
-                    $view_source = $html_view_source[0];
-                    $result[] = ["Company_Name" => $name, "Company_Url" => $link,"Location"=>$location,'View_Source'=>$view_source];  
-                }  
-            }
-            if(count($result) > 0){
-                try {
-                    $insertData = DB::table('dg_companies')->insert($result);
-                } catch (\Illuminate\Database\QueryException $ex) {
-                    echo $ex->getMessage();                  
+                if(count($result) > 0){
+                    try {
+                        $insertData = DB::table('dg_companies')->insert($result);
+                    } catch (\Illuminate\Database\QueryException $ex) {
+                        echo $ex->getMessage();                  
+                    }
                 }
+                $processed = TRUE;
             }
+            if($processed){
+                $url->status = 'processed';
+                $url->save();
+            }
+        }else{
+            UtilDebug::print_r_array("response", "No, Data For Processing");
         }
         UtilDebug::debug("End Processing");
     }
---
+++ app/DgUrl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DgUrl extends Model
+{
+    protected $table = "dg_urls";
+}
