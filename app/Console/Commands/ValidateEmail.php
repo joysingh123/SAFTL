a70733fbe97b3660b8ca08dea3a54a666492e0d3
joysingh123 <joy@salesaladin.com>
2018-11-17 13:29:04 +0530

    cron added

--- app/Console/Commands/ValidateEmail.php
+++ app/Console/Commands/ValidateEmail.php
@@ -95,7 +95,9 @@ class ValidateEmail extends Command {
                     }
                     if ($is_invalid) {
                         Emails::where('matched_contact_id', '=', $matched_id)->update(['status' => 'invalid']);
-                        MatchedContact::where('id', '=', $matched_id)->update(['email_status' => $v_response['email_status'], 'email_validation_date' => date("Y-m-d H:i:s")]);
+                        $email_status = $v_response['email_status'];
+                        $email_validation_date = date("Y-m-d H:i:s");
+                        MatchedContact::where('id', '=', $matched_id)->update(['email_status' => $email_status, 'email_validation_date' => $email_validation_date]);
                         $matched_contact = MatchedContact::where('id', '=', $matched_id)->first();
                         $contact_id = $matched_contact->contact_id;
                         $domain = $matched_contact->domain;
