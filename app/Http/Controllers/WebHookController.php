dd4f70bcd9d17f8615e3cea04fb6366629c4ac8a
joysingh123 <joy@salesaladin.com>
2018-11-19 13:40:07 +0530

    request updated

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -8,8 +8,9 @@ use App\Webhook;
 class WebHookController extends Controller
 {
     public function handle(Request $request){
+        $data =  $request->getContent();
         $webhook = new Webhook();
-        $webhook->response = $request;
+        $webhook->response = $data;
         $webhook->save();
     }
 }
