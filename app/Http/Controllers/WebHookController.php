2382cd377cc73345befcf38542d6b51550669c67
joysingh123 <joy@salesaladin.com>
2018-11-19 14:54:25 +0530

    email response added

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -8,9 +8,19 @@ use App\Webhook;
 class WebHookController extends Controller
 {
     public function handle(Request $request){
-        $data =  $request->getContent();
-        $webhook = new Webhook();
-        $webhook->response = $data;
-        $webhook->save();
+        $data = json_decode($request->getContent(), true);
+        if(count($data) > 0){
+            foreach ($data AS $d){
+                $email = $d->email;
+                $event = $d->event;
+                $reason = (isset($d->reason)) ? $d->reason : NULL;
+                $webhook = new Webhook();
+                $webhook->email = $email;
+                $webhook->email_status = $event;
+                $webhook->reason = $reason;
+                $webhook->response = $d;
+                $webhook->save();
+            }
+        }
     }
 }
