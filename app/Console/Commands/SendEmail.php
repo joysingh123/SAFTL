bde8c975f7c9159d82bc1bcdb1795120a30fb058
joysingh123 <joy@salesaladin.com>
2018-11-19 16:01:03 +0530

    contact status updated

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -7,6 +7,8 @@ use App\Helpers\UtilDebug;
 use App\Helpers\UtilString;
 use App\Mail\TestEmailViaSendgrid;
 use \Illuminate\Support\Facades\Mail;
+use App\Contacts;
+
 class SendEmail extends Command
 {
     /**
@@ -46,7 +48,21 @@ class SendEmail extends Command
         ini_set('mysql.connect_timeout', 600);
         ini_set('default_socket_timeout', 600);
         $data = ['message' => 'This is a test!'];
-        $response = Mail::to('ajay.agrawal9022@gmail.com')->send(new TestEmailViaSendgrid($data));
+        $limit = 5;
+        $contacts = Contacts::where('final','not processed')->take($limit)->get();
+        if($contacts->count() > 0){
+            foreach ($contacts AS $con){
+                $email = $con->email;
+                if(UtilString::is_email($email)){
+                    $response = Mail::to($email)->send(new TestEmailViaSendgrid($data));
+                    $con->final = "sent";
+                    $con->save();
+                }else{
+                    $con->final = "Email Not Exist";
+                    $con->save();
+                }
+            }
+        }   
         UtilDebug::debug("End processing");
     }
 }
