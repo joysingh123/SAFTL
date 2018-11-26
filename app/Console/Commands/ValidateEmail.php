6c1a0cae283cd39b16ce378d3478d4e689a35c05
joysingh123 <joy@salesaladin.com>
2018-11-26 12:25:06 +0530

    log added in api response

--- app/Console/Commands/ValidateEmail.php
+++ app/Console/Commands/ValidateEmail.php
@@ -73,6 +73,7 @@ class ValidateEmail extends Command {
                     $is_invalid = false;
                     foreach ($emails_array AS $email) {
                         $v_response = $this->validateEmail($email);
+                        print_r($v_response);
                         if ($v_response['email_status'] == 'valid' || $v_response['email_status'] == 'catch all') {
                             $is_invalid = false;
                             $email_status = $v_response['email_status'];
