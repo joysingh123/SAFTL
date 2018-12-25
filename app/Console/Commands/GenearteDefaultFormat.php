41f2f0264f1148e296db2cd1fa29932b920aa813
joysingh123 <joy@salesaladin.com>
2018-12-25 12:24:53 +0530

    default email format debug

--- app/Console/Commands/GenearteDefaultFormat.php
+++ app/Console/Commands/GenearteDefaultFormat.php
@@ -46,32 +46,33 @@ class GenearteDefaultFormat extends Command
                     ->select(DB::raw("distinct 0 AS user_id, CONCAT('FIRSTNAME.LASTNAME@',domain) AS email,domain AS company_domain,'FIRSTNAME' AS first_name,'LASTNAME' as last_name,'' as country,' ' as job_title,'' as status"))
                      ->where('email_format_available', '=', 'no')
                      ->get();
+        echo $matched_contacts_formate_1;
         
-        if($matched_contacts_formate_1->count() > 0){
-            $result = json_decode($matched_contacts_formate_1, true);
-            $insertData = DB::table('available_email')->insert($result);
-        }
-        
-        $matched_contacts_formate_2 = DB::table('matched_contacts')
-                    ->select(DB::raw("distinct 0 AS user_id, CONCAT('FLASTNAME@',domain) AS email,domain AS company_domain,'FIRSTNAME' AS first_name,'LASTNAME' as last_name,'' as country,' ' as job_title,'' as status"))
-                    ->where("email_format_available", '=', 'no')
-                    ->whereIn('employee_size',['501 to 1000','5001 to 10000','201 to 500','1001 to 5000','10000 above'])
-                    ->get();
-        
-        if($matched_contacts_formate_2->count() > 0){
-            $result = json_decode($matched_contacts_formate_2, true);
-            $insertData = DB::table('available_email')->insert($result);
-        }
-        
-        $matched_contacts_formate_3 = DB::table('matched_contacts')
-                    ->select(DB::raw("distinct 0 AS user_id, CONCAT('FIRSTNAME@',domain) AS email,domain AS company_domain,'FIRSTNAME' AS first_name,'LASTNAME' as last_name,'' as country,' ' as job_title,'' as status"))
-                    ->where('email_format_available', '=', 'no')
-                    ->whereIn('employee_size',['invalid','NA','1 to 10','11 to 50','51 to 200'])
-                    ->get();
-        if($matched_contacts_formate_3->count() > 0){
-            $result = json_decode($matched_contacts_formate_3, true);
-            $insertData = DB::table('available_email')->insert($result);
-        }
-        UtilDebug::debug("end processing");
+//        if($matched_contacts_formate_1->count() > 0){
+//            $result = json_decode($matched_contacts_formate_1, true);
+//            $insertData = DB::table('available_email')->insert($result);
+//        }
+//        
+//        $matched_contacts_formate_2 = DB::table('matched_contacts')
+//                    ->select(DB::raw("distinct 0 AS user_id, CONCAT('FLASTNAME@',domain) AS email,domain AS company_domain,'FIRSTNAME' AS first_name,'LASTNAME' as last_name,'' as country,' ' as job_title,'' as status"))
+//                    ->where("email_format_available", '=', 'no')
+//                    ->whereIn('employee_size',['501 to 1000','5001 to 10000','201 to 500','1001 to 5000','10000 above'])
+//                    ->get();
+//        
+//        if($matched_contacts_formate_2->count() > 0){
+//            $result = json_decode($matched_contacts_formate_2, true);
+//            $insertData = DB::table('available_email')->insert($result);
+//        }
+//        
+//        $matched_contacts_formate_3 = DB::table('matched_contacts')
+//                    ->select(DB::raw("distinct 0 AS user_id, CONCAT('FIRSTNAME@',domain) AS email,domain AS company_domain,'FIRSTNAME' AS first_name,'LASTNAME' as last_name,'' as country,' ' as job_title,'' as status"))
+//                    ->where('email_format_available', '=', 'no')
+//                    ->whereIn('employee_size',['invalid','NA','1 to 10','11 to 50','51 to 200'])
+//                    ->get();
+//        if($matched_contacts_formate_3->count() > 0){
+//            $result = json_decode($matched_contacts_formate_3, true);
+//            $insertData = DB::table('available_email')->insert($result);
+//        }
+//        UtilDebug::debug("end processing");
     }
 }
