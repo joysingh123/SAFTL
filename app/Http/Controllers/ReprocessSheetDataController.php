9f19522b7507e67a674b6810a05e38565dda30db
joysingh123 <joy@salesaladin.com>
2019-02-01 10:57:29 +0530

    reprocess sheet updated

--- app/Http/Controllers/ReprocessSheetDataController.php
+++ app/Http/Controllers/ReprocessSheetDataController.php
@@ -24,25 +24,26 @@ class ReprocessSheetDataController extends Controller
             $response['total'] = $sheet_data->count();
             $response['record update'] = 0;
             $linkedin_ids = $sheet_data->pluck('Company_Linkedin_ID');
-            $linkedin_ids_chunk = array_chunk($linkedin_ids, 1000);
-            foreach($linkedin_ids_chunk AS $lic){
-                $companies = CompaniesWithDomain::whereIn('linkedin_id',$lic)->get();
-                if($companies->count() > 0){
-                    foreach ($companies AS $sd){
-                        $linkedin_id = $sd->linkedin_id;
-                        $company_domain = $sd->company_domain;
-                        $updated = MasterUserContact::where('Company_Linkedin_ID',$linkedin_id)->where('Email_Status','domain not found')->update(['Company_Domain'=>$company_domain,'Email_Status'=>'domain found']);
-                        if($updated){
-                            $response['record update'] += $updated;
-                        }
-                    }
-                }
-            }
-            $sheet_data = MasterUserContact::where('Email_Status','domain found')->distinct()->get(['Sheet_ID']);
-            if($sheet_data->count() > 0){
-                $plucked_ids = $sheet_data->pluck('Sheet_ID');
-                MasterUserSheet::whereIn('ID', $plucked_ids)->update(['Status' => 'Under Processing']);
-            }
+            print_r($linkedin_ids);
+//            $linkedin_ids_chunk = array_chunk($linkedin_ids, 1000);
+//            foreach($linkedin_ids_chunk AS $lic){
+//                $companies = CompaniesWithDomain::whereIn('linkedin_id',$lic)->get();
+//                if($companies->count() > 0){
+//                    foreach ($companies AS $sd){
+//                        $linkedin_id = $sd->linkedin_id;
+//                        $company_domain = $sd->company_domain;
+//                        $updated = MasterUserContact::where('Company_Linkedin_ID',$linkedin_id)->where('Email_Status','domain not found')->update(['Company_Domain'=>$company_domain,'Email_Status'=>'domain found']);
+//                        if($updated){
+//                            $response['record update'] += $updated;
+//                        }
+//                    }
+//                }
+//            }
+//            $sheet_data = MasterUserContact::where('Email_Status','domain found')->distinct()->get(['Sheet_ID']);
+//            if($sheet_data->count() > 0){
+//                $plucked_ids = $sheet_data->pluck('Sheet_ID');
+//                MasterUserSheet::whereIn('ID', $plucked_ids)->update(['Status' => 'Under Processing']);
+//            }
         }
         print_r($response);
         UtilDebug::debug("End processing");
