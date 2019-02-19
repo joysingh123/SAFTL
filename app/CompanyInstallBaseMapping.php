fc9d15fb4c37cbf621cdddb417752a69f702eb1e
joysingh123 <joy@salesaladin.com>
2019-02-19 05:10:26 +0000

    company install updated

--- app/CompanyInstallBaseMapping.php
+++ app/CompanyInstallBaseMapping.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyInstallBaseMapping extends Model
 {
     protected $connection = 'mysql2';
-    protected $table = "company_install_base_mapping";
+    protected $table = "install_base_map";
 }
