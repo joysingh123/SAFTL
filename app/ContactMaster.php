0a3164c25de800c0dfd15390717d74476e9721c6
joysingh123 <joy@salesaladin.com>
2019-07-31 13:38:53 +0530

    company master

--- app/ContactMaster.php
+++ app/ContactMaster.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactMaster extends Model
 {
-    protected $connection = 'mysql3';
+    protected $connection = 'mysql2';
     protected $table = "contact_master";
     public $timestamps = false;
 }
