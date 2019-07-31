0476395a1f1bc3645a6db37df625b9c7375d7d61
joysingh123 <joy@salesaladin.com>
2019-07-31 13:00:44 +0530

    company added

--- app/CompanyMaster.php
+++ app/CompanyMaster.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyMaster extends Model
 {
+    protected $connection = 'mysql3';
     protected $table = "company_master";
     public $timestamps = false;
 }
