52b5bf06384d644f16756cbb3f45f32e3eda96f7
joysingh123 <joy@salesaladin.com>
2019-08-21 05:27:17 +0000

    database changed

--- app/CompanyGroup.php
+++ app/CompanyGroup.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyGroup extends Model
 {
-    protected $connection = 'mysql2';
+    protected $connection = 'mysql3';
     protected $table = "company_group";
 }
