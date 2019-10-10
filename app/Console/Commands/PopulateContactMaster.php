edafdaf9adad547a925164d38651b5e79782f7ed
joysingh123 <joy@salesaladin.com>
2019-10-10 13:42:26 +0530

    country stae city added

--- app/Console/Commands/PopulateContactMaster.php
+++ app/Console/Commands/PopulateContactMaster.php
@@ -70,6 +70,17 @@ class PopulateContactMaster extends Command
                 $title_level_id = 0;
                 $department_level_id = 0;
                 $country_id = 0;
+                $city = NULL;
+                $state = NULL;
+                $country = NULL;
+                if(!UtilString::is_empty_string($location)){
+                    if(UtilString::contains($location, ",")){
+                        $location_array = explode(",", $location);
+                        $city = (isset($location_array[0])) ? trim($location_array[0]) : NULL;
+                        $state = (isset($location_array[1])) ? trim($location_array[1]) : NULL;
+                        $country = (isset($location_array[2])) ? trim($location_array[2]) : NULL;
+                    }
+                }
                 $title_level_data = TitleLevelMaster::where('title_level',$title_level)->where('Status','Active');
                 $department_level_data = DepartmentMaster::where('Department',$department)->where('Status','Active');
                 $country_data = CountryMaster::where('Country Name',$contact_country)->where('Status','Active');
@@ -84,6 +95,30 @@ class PopulateContactMaster extends Command
                 }
                 $contact_exist = ContactMaster::where('first_name',$first_name)->where('last_name',$last_name)->where('domain',$domain)->get();
                 if($contact_exist->count() > 0){
+                    $company_data = CompanyMaster::where("domain",$domain)->get();
+                    $contact_exist->first()->full_name = (!UtilString::is_empty_string($full_name)) ? $full_name : NULL;
+                    $contact_exist->first()->first_name = $first_name;
+                    $contact_exist->first()->last_name = $last_name;
+                    $contact_exist->first()->email = $email;
+                    $contact_exist->first()->company_name = $company_name;
+                    $contact_exist->first()->job_title = $job_title;
+                    $contact_exist->first()->location = $location;
+                    $contact_exist->first()->city = $city;
+                    $contact_exist->first()->state = $state;
+                    $contact_exist->first()->country = $country;
+                    $contact_exist->first()->title_level = $title_level_id;
+                    $contact_exist->first()->s_title_level = $title_level;
+                    $contact_exist->first()->department = $department_level_id;
+                    $contact_exist->first()->s_department = $department;
+                    $contact_exist->first()->email_status = $email_status;
+                    $contact_exist->first()->email_validation_date = $email_validation_date;
+                    $contact_exist->first()->domain = $domain;
+                    $contact_exist->first()->company_id = 0;
+                    $contact_exist->first()->country_id = $country_id;
+                    if($company_data->count() > 0){
+                        $contact_exist->first()->company_id = $company_data->first()->id;
+                    }
+                    $contact_exist->first()->save();
                     $contact->populate_status = 'processed';
                     $contact->save();
                 }else{
@@ -96,8 +131,13 @@ class PopulateContactMaster extends Command
                     $con->company_name = $company_name;
                     $con->job_title = $job_title;
                     $con->location = $location;
+                    $con->city = $city;
+                    $con->state = $state;
+                    $con->country = $country;
                     $con->title_level = $title_level_id;
+                    $con->s_title_level = $title_level;
                     $con->department = $department_level_id;
+                    $con->s_department = $department;
                     $con->email_status = $email_status;
                     $con->email_validation_date = $email_validation_date;
                     $con->domain = $domain;
